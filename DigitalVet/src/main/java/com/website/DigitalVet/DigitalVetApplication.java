4a42075b1d2ec3f8b33bd723688b30b3dd635da3
denisaiacob <iacobdenisaalexandra49@gmail.com>
2023-04-29 09:53:43 +0300

    redux

---
+++ DigitalVet/src/main/java/com/website/DigitalVet/DigitalVetApplication.java
@@ -0,0 +1,13 @@
+package com.website.DigitalVet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalVetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalVetApplication.class, args);
+	}
+
+}
---
+++ DigitalVet/src/test/java/com/website/DigitalVet/DigitalVetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.DigitalVet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalVetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
