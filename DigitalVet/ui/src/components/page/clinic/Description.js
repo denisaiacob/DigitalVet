f2048a3bc93ead1a5040145706d353322f6f056e
denisaiacob <iacobdenisaalexandra49@gmail.com>
2023-05-09 18:39:15 +0300

    Clinic-description

---
+++ DigitalVet/ui/src/components/page/clinic/Description.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import {Box, Typography} from "@mui/material";
+
+function Description() {
+    return (
+        <Box
+            sx={{backgroundColor: "white"}}
+        >
+            <Box
+                sx={{
+                    textAlign: "start",
+                    borderTop: 20,
+                    borderBottom:50,
+                    borderColor: "white",
+                    marginLeft: 5
+                }}
+            >
+                <Typography
+                    variant="h6"
+                    fontWeight="bold"
+                >
+                    About us
+                </Typography>
+                <Typography
+                    sx={{marginTop:2}}
+                >
+                    Description
+                </Typography>
+            </Box>
+        </Box>
+    );
+}
+
+export default Description;
