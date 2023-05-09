9b01a89ea888cbe64ccf6a2e5d5d97c7dfe7623b
denisaiacob <iacobdenisaalexandra49@gmail.com>
2023-05-09 18:40:36 +0300

    margin

--- DigitalVet/ui/src/components/page/filtersResult/ShowClinics.js
+++ DigitalVet/ui/src/components/page/filtersResult/ShowClinics.js
@@ -6,10 +6,10 @@ import RightSide from "./RightSide";
 function ShowClinics() {
     return (
         <div className="container">
-            <div className="left-column" style={{marginLeft:40}}>
+            <div className="left-column" style={{marginLeft:40,marginBottom:90}}>
                 <LeftSide/>
             </div>
-            <div className="right-column" style={{marginLeft:60,marginRight:20}}>
+            <div className="right-column" style={{marginLeft:60,marginRight:20,marginBottom:20}}>
                 <RightSide/>
             </div>
         </div>
