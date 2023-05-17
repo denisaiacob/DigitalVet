a6087f51ba78140dff27d3af0a44706f174a3be4
denisaiacob <iacobdenisaalexandra49@gmail.com>
2023-05-17 15:41:24 +0300

    book button

--- DigitalVet/ui/src/components/page/filtersResult/ServiceDetailsBox.js
+++ DigitalVet/ui/src/components/page/filtersResult/ServiceDetailsBox.js
@@ -4,8 +4,9 @@ import {
     Grid,
     Typography,
     Box,
-    Paper
+    Paper, Button
 } from "@mui/material";
+import {Link, useHistory} from "react-router-dom";
 
 const RoundedTypography = styled(Typography)({
     fontFamily: 'Century Gothic',
@@ -15,20 +16,33 @@ const RoundedTypography = styled(Typography)({
 });
 
 function ServiceDetailsBox() {
+    const history = useHistory();
+
     return (
         <Paper variant="outlined">
-            <Box sx={{mt: 3, ml: 5, mr: 2, mb: 1}}>
+            <Box sx={{mt: 2, ml: 5, mr: 2, mb: 1}}>
                 <Grid container direction="column" spacing={2}>
-                    <Grid container direction="row" spacing={2}>
-                        <Grid item xs={11}>
+                    <Grid item container direction="row" spacing={2}>
+                        <Grid item xs={10.8}>
                             <RoundedTypography variant="h5">Name of the service</RoundedTypography>
                         </Grid>
-                        <Grid item xs={1}>
-                            <RoundedTypography variant="h5">15 Lei</RoundedTypography>
+                        <Grid item xs={1.2}>
+                            <RoundedTypography variant="h5">150 Lei</RoundedTypography>
                         </Grid>
                     </Grid>
-                    <Grid item>
-                        <Typography textAlign="start" variant="body2">Description</Typography>
+                    <Grid item container direction="row" spacing={2}>
+                        <Grid item xs={10.7}>
+                            <Typography textAlign="start" variant="body2">Description</Typography>
+                        </Grid>
+                        <Grid item xs={0.5}>
+                            <Button
+                                variant="outlined"
+                                style={{color:'#43ab98', borderColor: '#43ab98'}}
+                                component={Link} to="/service/${serviceId}"
+                            >
+                                Book
+                            </Button>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Box>
