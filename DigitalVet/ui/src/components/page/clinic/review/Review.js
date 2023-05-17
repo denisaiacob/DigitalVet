b785b5fa478136ca1471b2f31da3d6cccbb56d15
denisaiacob <iacobdenisaalexandra49@gmail.com>
2023-05-17 12:13:27 +0300

    Review

--- DigitalVet/ui/src/components/page/clinic/review/Review.js
+++ DigitalVet/ui/src/components/page/clinic/review/Review.js
@@ -1,35 +1,55 @@
 import * as React from 'react';
-import {Box, Grid} from "@mui/material";
-import FilterByRating from "./FilterByRating";
-import SummaryReviews from "./SummaryReviews";
+import {Box, Card, Chip, Divider, Grid, Rating, Stack, Typography} from "@mui/material";
+import {styled} from "@mui/material/styles";
+import {useState} from "react";
+
+const StyledTypography = styled(Typography)({
+    fontFamily: 'Optima',
+    fontWeight: 'bold',
+    fontSize: '1.0rem',
+    '@media (max-width:700px)': {
+        fontSize: '1.0rem',
+    },
+    textAlign: 'center',
+});
 
 function Review() {
+    const [average, setAverage] = useState(3);
 
     return (
-        <Box
-            sx={{backgroundColor: "white"}}
+        <Card
+            variant="outlined"
+            sx={{mt: 3}}
         >
-            <Box
-                sx={{
-                    textAlign: "start",
-                    borderTop: 20,
-                    borderBottom: 50,
-                    borderColor: "white",
-                    marginLeft: 3
-                }}
-            >
-                <Grid container spacing={2} direction="row">
-                    <Grid item>
-                        <SummaryReviews/>
+            <Box sx={{margin: 2, alignItems: 'center'}}>
+                <Grid container spacing={3}>
+                    <Grid item xs={3}>
+                        <Stack spacing={1} style={{textAlign: 'center'}}>
+                            <StyledTypography>Name</StyledTypography>
+                            <Typography style={{color: "grey", fontSize: '0.8rem'}}>16 May 2023</Typography>
+                        </Stack>
+                    </Grid>
+                    <Grid item xs={0.5}>
+                        <Divider orientation="vertical" variant="middle"></Divider>
                     </Grid>
                     <Grid item>
-                        <FilterByRating/>
+                        <Stack spacing={1}>
+                            <Stack direction='row' spacing={1}>
+                                <Rating
+                                    name="star-rating"
+                                    value={average}
+                                    readOnly
+                                />
+                                <Chip size='small' label="Service" />
+                                <Chip size='small' label="Vet" />
+                            </Stack>
+                            <Typography style={{fontSize:'0.9rem'}}>Text</Typography>
+                        </Stack>
                     </Grid>
                 </Grid>
             </Box>
-        </Box>
-    )
-        ;
+        </Card>
+    );
 }
 
 export default Review;
