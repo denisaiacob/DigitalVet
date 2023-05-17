09dacd85f9778435995a96f01de30744895b8323
denisaiacob <iacobdenisaalexandra49@gmail.com>
2023-05-17 16:38:35 +0300

    rename

--- DigitalVet/ui/src/components/page/clinic/services/Services.js
+++ DigitalVet/ui/src/components/page/clinic/services/Services.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {Box} from "@mui/material";
-import VetPart from "./vetPart";
+import VetPart from "./VetPart";
 import ServicePart from "./ServicePart";
 
 function Services() {
